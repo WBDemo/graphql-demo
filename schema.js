3b1572f76181f52b6459a1fabfc39993682d848f
lanxi <lanxi@mogujie.com>
2016-11-25 16:26:30 +0800

    feat: update step2

--- schema.js
+++ schema.js
@@ -3,26 +3,40 @@
  * @license: MIT
  * @copyright: Meili-inc.
  */
- import {
-   GraphQLObjectType,
-   GraphQLSchema,
-   GraphQLInt
- } from 'graphql';
+import {
+ GraphQLObjectType,
+ GraphQLSchema,
+ GraphQLInt
+} from 'graphql';
 
- let count = 0;
+let count = 0;
 
- let schema = new GraphQLSchema({
-   query: new GraphQLObjectType({
-     name: 'RootQueryType',
-     fields: {
-       count: {
-         type: GraphQLInt,
-         resolve: function() {
-           return count;
-         }
-       }
-     }
-   })
- });
+let schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      count: {
+        type: GraphQLInt,
+        description: 'The count!',
+        resolve: function() {
+          return count;
+        }
+      }
+    }
+  }),
+  mutation: new GraphQLObjectType({
+    name: 'RootMutationType',
+    fields: {
+      updateCount: {
+        type: GraphQLInt,
+        description: 'Update the count',
+        resolve: function() {
+          count += 1;
+          return count;
+        }
+      }
+    }
+  })
+});
 
- export default schema;
+export default schema;
--- server.js
+++ server.js
@@ -4,12 +4,23 @@
  * @copyright: Meili-inc.
  */
  import express from 'express';
+ import schema from './schema';
+
+ import { graphql } from 'graphql';
+ import bodyParser from 'body-parser';
 
  let app = express();
  let PORT = 3000;
 
+ //Parse post content as text
+ app.use(bodyParser.text({ type: 'application/graphql' }));
+
  app.post('/graphql', (req, res) => {
-   res.send('Hello!');
+   //GraphQL executor
+   graphql(schema, req.body)
+   .then((result) => {
+     res.send(JSON.stringify(result, null, 2));
+   })
  });
 
  let server = app.listen(PORT, function() {
